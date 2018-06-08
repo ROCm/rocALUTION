57520c3aef3e808340c4e5032560dad46f984f6a
Nico Trost <nico.trost@amd.com>
2018-06-08 13:01:41 +0200

    clang format

--- src/base/hip/hip_allocate_free.hpp
+++ src/base/hip/hip_allocate_free.hpp
@@ -6,22 +6,17 @@
 namespace rocalution {
 
 template <typename DataType>
-void allocate_hip(const int size, DataType **ptr);
+void allocate_hip(int size, DataType** ptr);
 
 template <typename DataType>
-void free_hip(DataType **ptr);
+void free_hip(DataType** ptr);
 
 template <typename DataType>
-void set_to_zero_hip(const int blocksize,
-                     const int max_threads,
-                     const int size, DataType *ptr);
+void set_to_zero_hip(int blocksize, int size, DataType* ptr);
 
 template <typename DataType>
-void set_to_one_hip(const int blocksize,
-                    const int max_threads,
-                    const int size, DataType *ptr);
+void set_to_one_hip(int blocksize, int size, DataType* ptr);
 
-
-}
+} // namespace rocalution
 
 #endif // ROCALUTION_HIP_ALLOCATE_FREE_HPP_
