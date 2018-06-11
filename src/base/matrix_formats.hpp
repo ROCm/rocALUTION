2845b37e3dea497ec851ad6f3d6262c28d2307cb
Nico Trost <nico.trost@amd.com>
2018-06-11 12:00:35 +0200

    clang format

--- src/base/matrix_formats.hpp
+++ src/base/matrix_formats.hpp
@@ -6,117 +6,119 @@
 namespace rocalution {
 
 /// Matrix Names
-const std::string _matrix_format_names [8] = {"DENSE", 
-                                              "CSR", 
-                                              "MCSR",
-                                              "BCSR",
-                                              "COO", 
-                                              "DIA", 
-                                              "ELL", 
-                                              "HYB"
-};
+const std::string _matrix_format_names[8] = {
+    "DENSE", "CSR", "MCSR", "BCSR", "COO", "DIA", "ELL", "HYB"};
 
 /// Matrix Enumeration
-enum _matrix_format {DENSE = 0, 
-                     CSR   = 1, 
-                     MCSR  = 2,
-                     BCSR  = 3,
-                     COO   = 4, 
-                     DIA   = 5, 
-                     ELL   = 6, 
-                     HYB   = 7};
-  
+enum _matrix_format
+{
+    DENSE = 0,
+    CSR   = 1,
+    MCSR  = 2,
+    BCSR  = 3,
+    COO   = 4,
+    DIA   = 5,
+    ELL   = 6,
+    HYB   = 7
+};
 
 /// Sparse Matrix -
 /// Sparse Compressed Row Format
 template <typename ValueType, typename IndexType>
-struct MatrixCSR {
-  /// Row offsets (row ptr)
-  IndexType *row_offset;
+struct MatrixCSR
+{
+    /// Row offsets (row ptr)
+    IndexType* row_offset;
 
-  /// Column index
-  IndexType *col;
+    /// Column index
+    IndexType* col;
 
-  /// Values
-  ValueType *val;
+    /// Values
+    ValueType* val;
 };
 
 /// Sparse Matrix -
 /// Modified Sparse Compressed Row Format
 template <typename ValueType, typename IndexType>
-struct MatrixMCSR {
-  /// Row offsets (row ptr)
-  IndexType *row_offset;
+struct MatrixMCSR
+{
+    /// Row offsets (row ptr)
+    IndexType* row_offset;
 
-  /// Column index
-  IndexType *col;
+    /// Column index
+    IndexType* col;
 
-  /// Values
-  ValueType *val;
+    /// Values
+    ValueType* val;
 };
 
 template <typename ValueType, typename IndexType>
-struct MatrixBCSR {
+struct MatrixBCSR
+{
 };
 
 /// Sparse Matrix -
 /// Coordinate Format
 template <typename ValueType, typename IndexType>
-struct MatrixCOO {
-  /// Row index
-  IndexType *row;
+struct MatrixCOO
+{
+    /// Row index
+    IndexType* row;
 
-  /// Column index
-  IndexType *col;
+    /// Column index
+    IndexType* col;
 
-  // Values
-  ValueType *val;
+    // Values
+    ValueType* val;
 };
 
 /// Sparse Matrix -
 /// Diagonal Format (see DIA_IND for indexing)
 template <typename ValueType, typename IndexType, typename Index = IndexType>
-struct MatrixDIA {
-  /// Number of diagonal
-  Index num_diag;
+struct MatrixDIA
+{
+    /// Number of diagonal
+    Index num_diag;
 
-  /// Offset with respect to the main diagonal
-  IndexType *offset;
+    /// Offset with respect to the main diagonal
+    IndexType* offset;
 
-  /// Values
-  ValueType *val;
+    /// Values
+    ValueType* val;
 };
 
 /// Sparse Matrix -
 /// ELL Format (see ELL_IND for indexing)
 template <typename ValueType, typename IndexType, typename Index = IndexType>
-struct MatrixELL {
-  /// Maximal elements per row
-  Index max_row;
+struct MatrixELL
+{
+    /// Maximal elements per row
+    Index max_row;
 
-  /// Column index
-  IndexType *col;
+    /// Column index
+    IndexType* col;
 
-  /// Values
-  ValueType *val;
+    /// Values
+    ValueType* val;
 };
 
 /// Sparse Matrix -
 /// Contains ELL and COO Matrices
 template <typename ValueType, typename IndexType, typename Index = IndexType>
-struct MatrixHYB {
-  MatrixELL<ValueType, IndexType, Index> ELL;
-  MatrixCOO<ValueType, IndexType> COO;
+struct MatrixHYB
+{
+    MatrixELL<ValueType, IndexType, Index> ELL;
+    MatrixCOO<ValueType, IndexType> COO;
 };
 
 /// Dense Matrix (see DENSE_IND for indexing)
 template <typename ValueType>
-struct MatrixDENSE {
-  /// Values
-  ValueType *val;
+struct MatrixDENSE
+{
+    /// Values
+    ValueType* val;
 };
 
-
-}
+} // namespace rocalution
 
 #endif // ROCALUTION_MATRIX_FORMATS_HPP_
--- src/base/matrix_formats_ind.hpp
+++ src/base/matrix_formats_ind.hpp
@@ -9,12 +9,12 @@
 
 // DENSE_IND_BASE == 0 - column-major
 // DENSE_IND_BASE == 1 - row-major
-#define DENSE_IND_BASE (DENSE_IND(2,2,10,0) == 22 ? 0 : 1)
+#define DENSE_IND_BASE (DENSE_IND(2, 2, 10, 0) == 22 ? 0 : 1)
 
 // ELL indexing
 #define ELL_IND_ROW(row, el, nrow, max_row) (el) * (nrow) + (row)
 #define ELL_IND_EL(row, el, nrow, max_row) (el) + (max_row) * (row)
-#define ELL_IND(row, el, nrow, max_row)  ELL_IND_ROW(row, el, nrow, max_row)
+#define ELL_IND(row, el, nrow, max_row) ELL_IND_ROW(row, el, nrow, max_row)
 
 // DIA indexing
 #define DIA_IND_ROW(row, el, nrow, ndiag) (el) * (nrow) + (row)
