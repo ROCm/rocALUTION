e5de3f0bdc5a320b46a5d0728cfd1b80bc0bf36a
Nico Trost <nico.trost@amd.com>
2018-06-13 15:18:07 +0200

    bad args test for LocalStencil class

---
+++ clients/tests/test_local_stencil.cpp
@@ -0,0 +1,62 @@
+/* ************************************************************************
+ * Copyright 2018 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+
+#include "testing_local_stencil.hpp"
+#include "utility.hpp"
+
+#include <gtest/gtest.h>
+/*
+typedef std::tuple<int, int, int, int, bool, int, bool> backend_tuple;
+
+int backend_rank[] = {-1, 0, 13};
+int backend_dev_node[] = {-1, 0, 13};
+int backend_dev[] = {-1, 0, 13};
+int backend_omp_threads[] = {-1, 0, 8};
+bool backend_affinity[] = {true, false};
+int backend_omp_threshold[] = {-1, 0, 20000};
+bool backend_disable_acc[] = {true, false};
+
+class parameterized_backend : public testing::TestWithParam<backend_tuple>
+{
+    protected:
+    parameterized_backend() {}
+    virtual ~parameterized_backend() {}
+    virtual void SetUp() {}
+    virtual void TearDown() {}
+};
+
+Arguments setup_backend_arguments(backend_tuple tup)
+{
+    Arguments arg;
+    arg.rank          = std::get<0>(tup);
+    arg.dev_per_node  = std::get<1>(tup);
+    arg.dev           = std::get<2>(tup);
+    arg.omp_nthreads  = std::get<3>(tup);
+    arg.omp_affinity  = std::get<4>(tup);
+    arg.omp_threshold = std::get<5>(tup);
+    arg.use_acc       = std::get<6>(tup);
+    return arg;
+}
+*/
+TEST(local_stencil_bad_args, local_stencil)
+{
+    testing_local_stencil_bad_args<float>();
+}
+/*
+TEST_P(parameterized_backend, backend)
+{
+    Arguments arg = setup_backend_arguments(GetParam());
+    testing_backend(arg);
+}
+
+INSTANTIATE_TEST_CASE_P(backend,
+                        parameterized_backend,
+                        testing::Combine(testing::ValuesIn(backend_rank),
+                                         testing::ValuesIn(backend_dev_node),
+                                         testing::ValuesIn(backend_dev),
+                                         testing::ValuesIn(backend_omp_threads),
+                                         testing::ValuesIn(backend_affinity),
+                                         testing::ValuesIn(backend_omp_threshold),
+                                         testing::ValuesIn(backend_disable_acc)));
+*/
