aabba1a84e1c6528c06b64203b5d15ff656c019c
Nico Trost <nico.trost@amd.com>
2018-06-14 18:21:38 +0200

    updated gmres example

--- clients/samples/gmres.cpp
+++ clients/samples/gmres.cpp
@@ -22,38 +22,41 @@ int main(int argc, char* argv[]) {
 
   LocalVector<double> x;
   LocalVector<double> rhs;
+  LocalVector<double> e;
 
   LocalMatrix<double> mat;
 
   mat.ReadFileMTX(std::string(argv[1]));
-
   mat.MoveToAccelerator();
   x.MoveToAccelerator();
   rhs.MoveToAccelerator();
+  e.MoveToAccelerator();
 
   x.Allocate("x", mat.GetN());
   rhs.Allocate("rhs", mat.GetM());
+  e.Allocate("e", mat.GetN());
 
   // Linear Solver
   GMRES<LocalMatrix<double>, LocalVector<double>, double > ls;
 
   // Preconditioner
-  MultiColoredSGS<LocalMatrix<double>, LocalVector<double>, double > p;
+  MultiColoredGS<LocalMatrix<double>, LocalVector<double>, double > p;
 
-  double tick, tack;
-
-  rhs.Ones();
+  e.Ones();
+  mat.Apply(e, &rhs);
   x.Zeros(); 
 
   ls.SetOperator(mat);
   ls.SetBasisSize(30);
   ls.SetPreconditioner(p);
-  //  ls.Verbose(2);
 
   ls.Build();
 
+  ls.Verbose(1);
+
   mat.Info();
 
+  double tick, tack;
   tick = rocalution_time();
 
   ls.Solve(rhs, &x);
@@ -62,6 +65,11 @@ int main(int argc, char* argv[]) {
   std::cout << "Solver execution:" << (tack-tick)/1000000 << " sec" << std::endl;
 
   ls.Clear();
+
+  e.ScaleAdd(-1.0, x);
+  double error = e.Norm();
+
+  std::cout << "||e - x||_2 = " << error << "\n";
 
   stop_rocalution();
 
