1eafd756ccd531b85b203610ad459d304c8073b9
Nico Trost <nico.trost@amd.com>
2018-06-15 09:29:53 +0200

    Vector::SetRandom() ==> Vector::SetRandomUniform() ; added Vector::SetRandomNormal() ; clang-format for base vector classes

--- src/base/global_vector.hpp
+++ src/base/global_vector.hpp
@@ -17,113 +17,115 @@ struct MRequest;
 
 // Global vector
 template <typename ValueType>
-class GlobalVector : public Vector<ValueType> {
+class GlobalVector : public Vector<ValueType>
+{
+    public:
+    GlobalVector();
+    GlobalVector(const ParallelManager& pm);
+    virtual ~GlobalVector();
 
-public:
+    virtual void MoveToAccelerator(void);
+    virtual void MoveToHost(void);
 
-  GlobalVector();
-  GlobalVector(const ParallelManager &pm);
-  virtual ~GlobalVector();
+    virtual void Info(void) const;
+    virtual bool Check(void) const;
 
-  virtual void MoveToAccelerator(void);
-  virtual void MoveToHost(void);
+    virtual IndexType2 GetSize(void) const;
+    virtual int GetLocalSize(void) const;
+    virtual int GetGhostSize(void) const;
 
-  virtual void Info(void) const;
-  virtual bool Check(void) const;
+    const LocalVector<ValueType>& GetInterior() const;
+    LocalVector<ValueType>& GetInterior();
+    const LocalVector<ValueType>& GetGhost() const;
 
-  virtual IndexType2 GetSize(void) const;
-  virtual int GetLocalSize(void) const;
-  virtual int GetGhostSize(void) const;
+    virtual void Allocate(std::string name, const IndexType2 size);
+    virtual void Clear(void);
 
-  const LocalVector<ValueType>& GetInterior() const;
-  LocalVector<ValueType>& GetInterior();
-  const LocalVector<ValueType>& GetGhost() const;
+    void SetParallelManager(const ParallelManager& pm);
 
-  virtual void Allocate(std::string name, const IndexType2 size);
-  virtual void Clear(void);
+    virtual void Zeros(void);
+    virtual void Ones(void);
+    virtual void SetValues(const ValueType val);
+    virtual void SetRandomUniform(unsigned long long seed,
+                                  ValueType a = static_cast<ValueType>(-1),
+                                  ValueType b = static_cast<ValueType>(1));
+    virtual void SetRandomNormal(unsigned long long seed,
+                                 ValueType mean = static_cast<ValueType>(0),
+                                 ValueType var  = static_cast<ValueType>(1));
+    void CloneFrom(const GlobalVector<ValueType>& src);
 
-  void SetParallelManager(const ParallelManager &pm);
+    // Accessing operator - only for host data
+    ValueType& operator[](const int i);
+    const ValueType& operator[](const int i) const;
 
-  virtual void Zeros(void);
-  virtual void Ones(void);
-  virtual void SetValues(const ValueType val);
-  virtual void SetRandom(const ValueType a = -1.0, const ValueType b = 1.0, const int seed = 0);
-  void CloneFrom(const GlobalVector<ValueType> &src);
+    void SetDataPtr(ValueType** ptr, std::string name, const IndexType2 size);
+    void LeaveDataPtr(ValueType** ptr);
 
-  // Accessing operator - only for host data
-  ValueType& operator[](const int i);
-  const ValueType& operator[](const int i) const;
+    virtual void CopyFrom(const GlobalVector<ValueType>& src);
+    virtual void ReadFileASCII(const std::string filename);
+    virtual void WriteFileASCII(const std::string filename) const;
+    virtual void ReadFileBinary(const std::string filename);
+    virtual void WriteFileBinary(const std::string filename) const;
 
-  void SetDataPtr(ValueType **ptr, std::string name, const IndexType2 size);
-  void LeaveDataPtr(ValueType **ptr);
+    // this = this + alpha*x
+    virtual void AddScale(const GlobalVector<ValueType>& x, const ValueType alpha);
+    // this = alpha*this + x
+    virtual void ScaleAdd(const ValueType alpha, const GlobalVector<ValueType>& x);
+    // this = alpha*this + x*beta + y*gamma
+    virtual void ScaleAdd2(const ValueType alpha,
+                           const GlobalVector<ValueType>& x,
+                           const ValueType beta,
+                           const GlobalVector<ValueType>& y,
+                           const ValueType gamma);
+    // this = alpha*this
+    virtual void Scale(const ValueType alpha);
+    // this^T x
+    virtual ValueType Dot(const GlobalVector<ValueType>& x) const;
+    // this^T x
+    virtual ValueType DotNonConj(const GlobalVector<ValueType>& x) const;
+    // sqrt(this^T this)
+    virtual ValueType Norm(void) const;
+    // reduce
+    virtual ValueType Reduce(void) const;
+    // L1 norm, sum(|this|)
+    virtual ValueType Asum(void) const;
+    // Amax, max(|this|)
+    virtual int Amax(ValueType& value) const;
+    // point-wise multiplication
+    virtual void PointWiseMult(const GlobalVector<ValueType>& x);
+    virtual void PointWiseMult(const GlobalVector<ValueType>& x, const GlobalVector<ValueType>& y);
 
-  virtual void CopyFrom(const GlobalVector<ValueType> &src);
-  virtual void ReadFileASCII(const std::string filename);
-  virtual void WriteFileASCII(const std::string filename) const;
-  virtual void ReadFileBinary(const std::string filename);
-  virtual void WriteFileBinary(const std::string filename) const;
+    virtual void Power(const double power);
 
-  // this = this + alpha*x
-  virtual void AddScale(const GlobalVector<ValueType> &x, const ValueType alpha);
-  // this = alpha*this + x
-  virtual void ScaleAdd(const ValueType alpha, const GlobalVector<ValueType> &x);
-  // this = alpha*this + x*beta + y*gamma
-  virtual void ScaleAdd2(const ValueType alpha, const GlobalVector<ValueType> &x,
-                         const ValueType beta, const GlobalVector<ValueType> &y,
-                         const ValueType gamma);
-  // this = alpha*this
-  virtual void Scale(const ValueType alpha);
-  // this^T x
-  virtual ValueType Dot(const GlobalVector<ValueType> &x) const;
-  // this^T x
-  virtual ValueType DotNonConj(const GlobalVector<ValueType> &x) const;
-  // sqrt(this^T this)
-  virtual ValueType Norm(void) const;
-  // reduce
-  virtual ValueType Reduce(void) const;
-  // L1 norm, sum(|this|)
-  virtual ValueType Asum(void) const;
-  // Amax, max(|this|)
-  virtual int Amax(ValueType &value) const;
-  // point-wise multiplication
-  virtual void PointWiseMult(const GlobalVector<ValueType> &x);
-  virtual void PointWiseMult(const GlobalVector<ValueType> &x, const GlobalVector<ValueType> &y);
+    // Restriction operator based on restriction mapping vector
+    void Restriction(const GlobalVector<ValueType>& vec_fine, const LocalVector<int>& map);
 
-  virtual void Power(const double power);
+    // Prolongation operator based on restriction(!) mapping vector
+    void Prolongation(const GlobalVector<ValueType>& vec_coarse, const LocalVector<int>& map);
 
-  // Restriction operator based on restriction mapping vector
-  void Restriction(const GlobalVector<ValueType> &vec_fine, const LocalVector<int> &map);
+    protected:
+    virtual bool is_host(void) const;
+    virtual bool is_accel(void) const;
 
-  // Prolongation operator based on restriction(!) mapping vector
-  void Prolongation(const GlobalVector<ValueType> &vec_coarse, const LocalVector<int> &map);
+    void UpdateGhostValuesAsync(const GlobalVector<ValueType>& in);
+    void UpdateGhostValuesSync(void);
 
-protected:
+    private:
+    MRequest* recv_event_;
+    MRequest* send_event_;
 
-  virtual bool is_host(void) const;
-  virtual bool is_accel(void) const;
+    ValueType* recv_boundary_;
+    ValueType* send_boundary_;
 
-  void UpdateGhostValuesAsync(const GlobalVector<ValueType> &in);
-  void UpdateGhostValuesSync(void);
+    LocalVector<ValueType> vector_interior_;
+    LocalVector<ValueType> vector_ghost_;
 
-private:
+    friend class LocalMatrix<ValueType>;
+    friend class GlobalMatrix<ValueType>;
 
-  MRequest *recv_event_;
-  MRequest *send_event_;
-
-  ValueType *recv_boundary_;
-  ValueType *send_boundary_;
-
-  LocalVector<ValueType> vector_interior_;
-  LocalVector<ValueType> vector_ghost_;
-
-  friend class LocalMatrix<ValueType>;
-  friend class GlobalMatrix<ValueType>;
-
-  friend class BaseRocalution<ValueType>;
-
+    friend class BaseRocalution<ValueType>;
 };
 
-
-}
+} // namespace rocalution
 
 #endif // ROCALUTION_GLOBAL_VECTOR_HPP_
--- src/base/vector.cpp
+++ src/base/vector.cpp
@@ -9,324 +9,314 @@
 namespace rocalution {
 
 template <typename ValueType>
-Vector<ValueType>::Vector() {
+Vector<ValueType>::Vector()
+{
+    LOG_DEBUG(this, "Vector::Vector()", "default constructor");
 
-  LOG_DEBUG(this, "Vector::Vector()",
-            "default constructor");
-
-  this->object_name_ = "";
-
+    this->object_name_ = "";
 }
 
 template <typename ValueType>
-Vector<ValueType>::~Vector() {
-
-  LOG_DEBUG(this, "Vector::~Vector()",
-            "default destructor");
-
+Vector<ValueType>::~Vector()
+{
+    LOG_DEBUG(this, "Vector::~Vector()", "default destructor");
 }
 
 template <typename ValueType>
-int Vector<ValueType>::GetLocalSize(void) const {
-
-  return IndexTypeToInt(this->GetSize());
-
+int Vector<ValueType>::GetLocalSize(void) const
+{
+    return IndexTypeToInt(this->GetSize());
 }
 
 template <typename ValueType>
-int Vector<ValueType>::GetGhostSize(void) const {
-
-  return 0;
-
+int Vector<ValueType>::GetGhostSize(void) const
+{
+    return 0;
 }
 
 template <typename ValueType>
-void Vector<ValueType>::CopyFrom(const LocalVector<ValueType> &src) {
-
-  LOG_INFO("Vector<ValueType>::CopyFrom(const LocalVector<ValueType> &src)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  src.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::CopyFrom(const LocalVector<ValueType>& src)
+{
+    LOG_INFO("Vector<ValueType>::CopyFrom(const LocalVector<ValueType> &src)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    src.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-void Vector<ValueType>::CopyFrom(const GlobalVector<ValueType> &src) {
-
-  LOG_INFO("Vector<ValueType>::CopyFrom(const GlobalVector<ValueType> &src)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  src.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::CopyFrom(const GlobalVector<ValueType>& src)
+{
+    LOG_INFO("Vector<ValueType>::CopyFrom(const GlobalVector<ValueType> &src)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    src.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-void Vector<ValueType>::CopyFromAsync(const LocalVector<ValueType> &src) {
-
-  LOG_INFO("Vector<ValueType>::CopyFromAsync(const LocalVector<ValueType> &src)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  src.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::CopyFromAsync(const LocalVector<ValueType>& src)
+{
+    LOG_INFO("Vector<ValueType>::CopyFromAsync(const LocalVector<ValueType> &src)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    src.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-void Vector<ValueType>::CopyFromFloat(const LocalVector<float> &src) {
-
-  LOG_INFO("Vector<ValueType>::CopyFromFloat(const LocalVector<float> &src)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  src.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::CopyFromFloat(const LocalVector<float>& src)
+{
+    LOG_INFO("Vector<ValueType>::CopyFromFloat(const LocalVector<float> &src)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    src.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-void Vector<ValueType>::CopyFromDouble(const LocalVector<double> &src) {
-
-  LOG_INFO("Vector<ValueType>::CopyFromDouble(const LocalVector<double> &src)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  src.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::CopyFromDouble(const LocalVector<double>& src)
+{
+    LOG_INFO("Vector<ValueType>::CopyFromDouble(const LocalVector<double> &src)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    src.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-void Vector<ValueType>::CloneFrom(const LocalVector<ValueType> &src) {
-
-  LOG_INFO("Vector<ValueType>::CloneFrom(const LocalVector<ValueType> &src)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  src.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::CloneFrom(const LocalVector<ValueType>& src)
+{
+    LOG_INFO("Vector<ValueType>::CloneFrom(const LocalVector<ValueType> &src)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    src.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-void Vector<ValueType>::CloneFrom(const GlobalVector<ValueType> &src) {
-
-  LOG_INFO("Vector<ValueType>::CloneFrom(const GlobalVector<ValueType> &src)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  src.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::CloneFrom(const GlobalVector<ValueType>& src)
+{
+    LOG_INFO("Vector<ValueType>::CloneFrom(const GlobalVector<ValueType> &src)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    src.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-void Vector<ValueType>::AddScale(const LocalVector<ValueType> &x, const ValueType alpha) {
-
-  LOG_INFO("Vector<ValueType>::AddScale(const LocalVector<ValueType> &x, const ValueType alpha)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  x.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::AddScale(const LocalVector<ValueType>& x, const ValueType alpha)
+{
+    LOG_INFO("Vector<ValueType>::AddScale(const LocalVector<ValueType> &x, const ValueType alpha)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    x.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-void Vector<ValueType>::AddScale(const GlobalVector<ValueType> &x, const ValueType alpha) {
-
-  LOG_INFO("Vector<ValueType>::AddScale(const GlobalVector<ValueType> &x, const ValueType alpha)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  x.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::AddScale(const GlobalVector<ValueType>& x, const ValueType alpha)
+{
+    LOG_INFO(
+        "Vector<ValueType>::AddScale(const GlobalVector<ValueType> &x, const ValueType alpha)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    x.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-void Vector<ValueType>::ScaleAdd(const ValueType alpha, const LocalVector<ValueType> &x) {
-
-  LOG_INFO("Vector<ValueType>::ScaleAdd(const ValueType alpha, const LocalVector<ValueType> &x)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  x.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::ScaleAdd(const ValueType alpha, const LocalVector<ValueType>& x)
+{
+    LOG_INFO("Vector<ValueType>::ScaleAdd(const ValueType alpha, const LocalVector<ValueType> &x)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    x.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-void Vector<ValueType>::ScaleAdd(const ValueType alpha, const GlobalVector<ValueType> &x) {
-
-  LOG_INFO("Vector<ValueType>::ScaleAdd(const ValueType alpha, const GlobalVector<ValueType> &x)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  x.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::ScaleAdd(const ValueType alpha, const GlobalVector<ValueType>& x)
+{
+    LOG_INFO(
+        "Vector<ValueType>::ScaleAdd(const ValueType alpha, const GlobalVector<ValueType> &x)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    x.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-ValueType Vector<ValueType>::Dot(const LocalVector<ValueType> &x) const {
-
-  LOG_INFO("Vector<ValueType>::Dot(const LocalVector<ValueType> &x) const");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  x.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+ValueType Vector<ValueType>::Dot(const LocalVector<ValueType>& x) const
+{
+    LOG_INFO("Vector<ValueType>::Dot(const LocalVector<ValueType> &x) const");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    x.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-ValueType Vector<ValueType>::Dot(const GlobalVector<ValueType> &x) const {
-
-  LOG_INFO("Vector<ValueType>::Dot(const GlobalVector<ValueType> &x) const");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  x.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+ValueType Vector<ValueType>::Dot(const GlobalVector<ValueType>& x) const
+{
+    LOG_INFO("Vector<ValueType>::Dot(const GlobalVector<ValueType> &x) const");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    x.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-ValueType Vector<ValueType>::DotNonConj(const LocalVector<ValueType> &x) const {
-
-  LOG_INFO("Vector<ValueType>::DotNonConj(const LocalVector<ValueType> &x) const");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  x.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+ValueType Vector<ValueType>::DotNonConj(const LocalVector<ValueType>& x) const
+{
+    LOG_INFO("Vector<ValueType>::DotNonConj(const LocalVector<ValueType> &x) const");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    x.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-ValueType Vector<ValueType>::DotNonConj(const GlobalVector<ValueType> &x) const {
-
-  LOG_INFO("Vector<ValueType>::DotNonConj(const GlobalVector<ValueType> &x) const");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  x.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+ValueType Vector<ValueType>::DotNonConj(const GlobalVector<ValueType>& x) const
+{
+    LOG_INFO("Vector<ValueType>::DotNonConj(const GlobalVector<ValueType> &x) const");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    x.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-void Vector<ValueType>::PointWiseMult(const LocalVector<ValueType> &x) {
-
-  LOG_INFO("Vector<ValueType>::PointWiseMult(const LocalVector<ValueType> &x)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  x.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::PointWiseMult(const LocalVector<ValueType>& x)
+{
+    LOG_INFO("Vector<ValueType>::PointWiseMult(const LocalVector<ValueType> &x)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    x.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-void Vector<ValueType>::PointWiseMult(const GlobalVector<ValueType> &x) {
-
-  LOG_INFO("Vector<ValueType>::PointWiseMult(const GlobalVector<ValueType> &x)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  x.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::PointWiseMult(const GlobalVector<ValueType>& x)
+{
+    LOG_INFO("Vector<ValueType>::PointWiseMult(const GlobalVector<ValueType> &x)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    x.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-void Vector<ValueType>::PointWiseMult(const LocalVector<ValueType> &x, const LocalVector<ValueType> &y) {
-
-  LOG_INFO("Vector<ValueType>::PointWiseMult(const LocalVector<ValueType> &x, const LocalVector<ValueType> &y)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  x.Info();
-  y.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::PointWiseMult(const LocalVector<ValueType>& x,
+                                      const LocalVector<ValueType>& y)
+{
+    LOG_INFO("Vector<ValueType>::PointWiseMult(const LocalVector<ValueType> &x, const "
+             "LocalVector<ValueType> &y)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    x.Info();
+    y.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-void Vector<ValueType>::PointWiseMult(const GlobalVector<ValueType> &x, const LocalVector<ValueType> &y) {
-
-  LOG_INFO("Vector<ValueType>::PointWiseMult(const GlobalVector<ValueType> &x, const LocalVector<ValueType> &y)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  x.Info();
-  y.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::PointWiseMult(const GlobalVector<ValueType>& x,
+                                      const LocalVector<ValueType>& y)
+{
+    LOG_INFO("Vector<ValueType>::PointWiseMult(const GlobalVector<ValueType> &x, const "
+             "LocalVector<ValueType> &y)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    x.Info();
+    y.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-void Vector<ValueType>::ScaleAddScale(const ValueType alpha, const LocalVector<ValueType> &x, const ValueType beta) {
-
-  LOG_INFO("ScaleAddScale(const ValueType alpha, const LocalVector<ValueType> &x, const ValueType beta)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  x.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::ScaleAddScale(const ValueType alpha,
+                                      const LocalVector<ValueType>& x,
+                                      const ValueType beta)
+{
+    LOG_INFO("ScaleAddScale(const ValueType alpha, const LocalVector<ValueType> &x, const "
+             "ValueType beta)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    x.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-void Vector<ValueType>::ScaleAddScale(const ValueType alpha, const GlobalVector<ValueType> &x, const ValueType beta) {
-
-  LOG_INFO("ScaleAddScale(const ValueType alpha, const GlobalVector<ValueType> &x, const ValueType beta)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  x.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::ScaleAddScale(const ValueType alpha,
+                                      const GlobalVector<ValueType>& x,
+                                      const ValueType beta)
+{
+    LOG_INFO("ScaleAddScale(const ValueType alpha, const GlobalVector<ValueType> &x, const "
+             "ValueType beta)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    x.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-void Vector<ValueType>::ScaleAdd2(const ValueType alpha, const LocalVector<ValueType> &x, const ValueType beta, const LocalVector<ValueType> &y, const ValueType gamma) {
-
-  LOG_INFO("ScaleAdd2(const ValueType alpha, const LocalVector<ValueType> &x, const ValueType beta, const LocalVector<ValueType> &y, const ValueType gamma)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  x.Info();
-  y.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::ScaleAdd2(const ValueType alpha,
+                                  const LocalVector<ValueType>& x,
+                                  const ValueType beta,
+                                  const LocalVector<ValueType>& y,
+                                  const ValueType gamma)
+{
+    LOG_INFO("ScaleAdd2(const ValueType alpha, const LocalVector<ValueType> &x, const ValueType "
+             "beta, const LocalVector<ValueType> &y, const ValueType gamma)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    x.Info();
+    y.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-void Vector<ValueType>::ScaleAdd2(const ValueType alpha, const GlobalVector<ValueType> &x,const ValueType beta, const GlobalVector<ValueType> &y, const ValueType gamma) {
-
-  LOG_INFO("ScaleAdd2(const ValueType alpha, const GlobalVector<ValueType> &x,const ValueType beta, const GlobalVector<ValueType> &y, const ValueType gamma)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  x.Info();
-  y.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::ScaleAdd2(const ValueType alpha,
+                                  const GlobalVector<ValueType>& x,
+                                  const ValueType beta,
+                                  const GlobalVector<ValueType>& y,
+                                  const ValueType gamma)
+{
+    LOG_INFO("ScaleAdd2(const ValueType alpha, const GlobalVector<ValueType> &x, const ValueType "
+             "beta, const GlobalVector<ValueType> &y, const ValueType gamma)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    x.Info();
+    y.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-void Vector<ValueType>::ExclusiveScan(const LocalVector<ValueType> &x) {
-
-  LOG_INFO("ExclusiveScan(const LocalVector<ValueType> &x)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  x.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::ExclusiveScan(const LocalVector<ValueType>& x)
+{
+    LOG_INFO("ExclusiveScan(const LocalVector<ValueType> &x)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    x.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
 
 template <typename ValueType>
-void Vector<ValueType>::ExclusiveScan(const GlobalVector<ValueType> &x) {
-
-  LOG_INFO("ExclusiveScan(const GlobalVector<ValueType> &x)");
-  LOG_INFO("Mismatched types:");
-  this->Info();
-  x.Info();
-  FATAL_ERROR(__FILE__, __LINE__);
-
+void Vector<ValueType>::ExclusiveScan(const GlobalVector<ValueType>& x)
+{
+    LOG_INFO("ExclusiveScan(const GlobalVector<ValueType> &x)");
+    LOG_INFO("Mismatched types:");
+    this->Info();
+    x.Info();
+    FATAL_ERROR(__FILE__, __LINE__);
 }
-
 
 template class Vector<double>;
 template class Vector<float>;
 #ifdef SUPPORT_COMPLEX
-template class Vector<std::complex<double> >;
-template class Vector<std::complex<float> >;
+template class Vector<std::complex<double>>;
+template class Vector<std::complex<float>>;
 #endif
 
 template class Vector<int>;
 
-}
+} // namespace rocalution
