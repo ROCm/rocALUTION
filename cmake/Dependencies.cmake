0462a03928b84c8efb42bcbe4de4d7c427fe049b
Nico Trost <nico.trost@amd.com>
2018-07-27 15:04:04 +0200

    rocPRIM added in build chain

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -43,6 +43,49 @@ if(SUPPORT_HIP)
   endif()
 endif()
 
+# rocPRIM package
+if(SUPPORT_HIP)
+  if(HIP_PLATFORM STREQUAL "hcc")
+    find_package(ROCPRIM QUIET CONFIG PATHS /opt/rocm)
+    find_package(HIPCUB QUIET CONFIG PATHS /opt/rocm)
+    if(NOT ROCPRIM_FOUND)
+      set(ROCPRIM_ROOT ${CMAKE_CURRENT_BINARY_DIR}/rocPRIM CACHE PATH "")
+      message(STATUS "Downloading rocPRIM.")
+      download_project(PROJ    rocPRIM
+           GIT_REPOSITORY      https://github.com/ROCmSoftwarePlatform/rocPRIM.git
+           GIT_TAG             master
+           INSTALL_DIR         ${ROCPRIM_ROOT}
+           CMAKE_ARGS          -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_TEST=OFF -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DCMAKE_CXX_COMPILER=/opt/rocm/bin/hcc
+           LOG_DOWNLOAD        TRUE
+           LOG_CONFIGURE       TRUE
+           LOG_INSTALL         TRUE
+           BUILD_PROJECT       TRUE
+           UPDATE_DISCONNECT   TRUE
+      )
+    find_package(ROCPRIM REQUIRED CONFIG PATHS ${ROCPRIM_ROOT})
+    find_package(HIPCUB REQUIRED CONFIG PATHS ${ROCPRIM_ROOT})
+    endif()
+  elseif(HIP_PLATFORM STREQUAL "nvcc")
+    find_package(HIPCUB QUIET CONFIG PATHS /opt/rocm)
+    if(NOT HIPCUB_FOUND)
+      set(ROCPRIM_ROOT ${CMAKE_CURRENT_BINARY_DIR}/rocPRIM CACHE PATH "")
+      message(STATUS "Downloading rocPRIM.")
+      download_project(PROJ    rocPRIM
+           GIT_REPOSITORY      https://github.com/ROCmSoftwarePlatform/rocPRIM.git
+           GIT_TAG             master
+           INSTALL_DIR         ${ROCPRIM_ROOT}
+           CMAKE_ARGS          -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_TEST=OFF -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DCMAKE_CXX_COMPILER=${HIP_HIPCC_EXECUTABLE}
+           LOG_DOWNLOAD        TRUE
+           LOG_CONFIGURE       TRUE
+           LOG_INSTALL         TRUE
+           BUILD_PROJECT       TRUE
+           UPDATE_DISCONNECT   TRUE
+      )
+    find_package(HIPCUB REQUIRED CONFIG PATHS ${ROCPRIM_ROOT})
+    endif()
+  endif()
+endif()
+
 # Test dependencies
 if(BUILD_TEST)
   if(NOT DEPENDENCIES_FORCE_DOWNLOAD)
@@ -65,40 +108,7 @@ if(BUILD_TEST)
     )
   endif()
   find_package(GTest REQUIRED)
-#  # Download some test matrices
-#  set(TEST_MATRICES
-#    nos1
-#    nos2
-#    nos3
-#    nos4
-#    nos5
-#    nos6
-#    nos7
-#  )
-#  foreach(m ${TEST_MATRICES})
-#    if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/matrices/${m}.mtx")
-#      file(DOWNLOAD ftp://math.nist.gov/pub/MatrixMarket2/Harwell-Boeing/lanpro/${m}.mtx.gz
-#           ${CMAKE_CURRENT_BINARY_DIR}/matrices/${m}.mtx.gz)
-#      execute_process(COMMAND gzip -d -f ${m}.mtx.gz
-#                      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/matrices)
-#    endif()
-#  endforeach()
 endif()
-
-# rocPRIM package
-#set(ROCPRIM_ROOT ${CMAKE_CURRENT_BINARY_DIR}/rocPRIM CACHE PATH "")
-#message(STATUS "Downloading rocPRIM.")
-#download_project(PROJ    rocPRIM
-#     GIT_REPOSITORY      https://github.com/ROCmSoftwarePlatform/rocPRIM.git
-#     GIT_TAG             master
-#     INSTALL_DIR         ${ROCPRIM_ROOT}
-#     CMAKE_ARGS          -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_TEST=OFF -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-#     LOG_DOWNLOAD        TRUE
-#     LOG_CONFIGURE       TRUE
-#     LOG_INSTALL         TRUE
-#     BUILD_PROJECT       TRUE
-#     UPDATE_DISCONNECT   TRUE
-#)
 
 # ROCm package
 find_package(ROCM QUIET CONFIG PATHS /opt/rocm)
