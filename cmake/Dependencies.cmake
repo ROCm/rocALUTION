856e3488661ed27bcc439cd0d3cf28712442178e
Nico Trost <nico.trost@amd.com>
2018-11-22 11:37:01 +0100

    fix hip packaging directory for hip runtime

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -29,15 +29,16 @@ find_package(Git REQUIRED)
 # DownloadProject package
 include(cmake/DownloadProject/DownloadProject.cmake)
 
+# Workaround until hcc & hip cmake modules fixes symlink logic in their config files.
+# (Thanks to rocBLAS devs for finding workaround for this problem!)
+list(APPEND CMAKE_PREFIX_PATH /opt/rocm/hcc /opt/rocm/hip /opt/rocm)
+
 if(SUPPORT_HIP)
   # HIP configuration
   if(HIP_PLATFORM STREQUAL "hcc")
-    # Workaround until hcc & hip cmake modules fixes symlink logic in their config files.
-    # (Thanks to rocBLAS devs for finding workaround for this problem!)
-    list(APPEND CMAKE_PREFIX_PATH /opt/rocm/hcc /opt/rocm/hip)
     # Ignore hcc warning: argument unused during compilation: '-isystem /opt/rocm/hip/include'
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-unused-command-line-argument")
-    find_package(HIP REQUIRED CONFIG PATHS /opt/rocm)
+    find_package(HIP REQUIRED CONFIG PATHS ${CMAKE_PREFIX_PATH})
     set(CMAKE_SHARED_LIBRARY_CREATE_HIP_FLAGS ${CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS})
     set(CMAKE_HIP_COMPILE_OPTIONS_PIC ${CMAKE_C_COMPILE_OPTIONS_PIC})
     set(CMAKE_HIP_COMPILE_OPTIONS_PIE ${CMAKE_C_COMPILE_OPTIONS_PIE})
@@ -65,8 +66,8 @@ endif()
 # rocPRIM package
 if(SUPPORT_HIP)
   if(HIP_PLATFORM STREQUAL "hcc")
-    find_package(ROCPRIM QUIET CONFIG PATHS /opt/rocm)
-    find_package(HIPCUB QUIET CONFIG PATHS /opt/rocm)
+    find_package(ROCPRIM QUIET CONFIG PATHS ${CMAKE_PREFIX_PATH})
+    find_package(HIPCUB QUIET CONFIG PATHS ${CMAKE_PREFIX_PATH})
     if(NOT ROCPRIM_FOUND)
       set(ROCPRIM_ROOT ${CMAKE_CURRENT_BINARY_DIR}/rocPRIM CACHE PATH "")
       message(STATUS "Downloading rocPRIM.")
@@ -74,7 +75,7 @@ if(SUPPORT_HIP)
            GIT_REPOSITORY      https://github.com/ROCmSoftwarePlatform/rocPRIM.git
            GIT_TAG             master
            INSTALL_DIR         ${ROCPRIM_ROOT}
-           CMAKE_ARGS          -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_TEST=OFF -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DCMAKE_CXX_COMPILER=/opt/rocm/bin/hcc
+           CMAKE_ARGS          -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_TEST=OFF -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DCMAKE_CXX_COMPILER=${HIP_HCC_EXECUTABLE}
            LOG_DOWNLOAD        TRUE
            LOG_CONFIGURE       TRUE
            LOG_INSTALL         TRUE
@@ -85,7 +86,7 @@ if(SUPPORT_HIP)
     find_package(HIPCUB REQUIRED CONFIG PATHS ${ROCPRIM_ROOT})
     endif()
   elseif(HIP_PLATFORM STREQUAL "nvcc")
-    find_package(HIPCUB QUIET CONFIG PATHS /opt/rocm)
+    find_package(HIPCUB QUIET CONFIG PATHS ${CMAKE_PREFIX_PATH})
     if(NOT HIPCUB_FOUND)
       set(ROCPRIM_ROOT ${CMAKE_CURRENT_BINARY_DIR}/rocPRIM CACHE PATH "")
       message(STATUS "Downloading rocPRIM.")
@@ -130,7 +131,7 @@ if(BUILD_TEST)
 endif()
 
 # ROCm package
-find_package(ROCM QUIET CONFIG PATHS /opt/rocm)
+find_package(ROCM QUIET CONFIG PATHS ${CMAKE_PREFIX_PATH})
 if(NOT ROCM_FOUND)
   set(rocm_cmake_tag "master" CACHE STRING "rocm-cmake tag to download")
   file(DOWNLOAD https://github.com/RadeonOpenCompute/rocm-cmake/archive/${rocm_cmake_tag}.zip
--- cmake/SetToolchain.cmake
+++ cmake/SetToolchain.cmake
@@ -50,5 +50,25 @@ else()
   if (SUPPORT_HIP)
     find_package(ROCBLAS 0.14.2 REQUIRED) # ROCm 1.9
     find_package(ROCSPARSE 0.1.3 REQUIRED) # ROCm 1.9
+
+    # Find HCC executable
+    find_program(
+        HIP_HCC_EXECUTABLE
+        NAMES hcc
+        PATHS
+        "${HIP_ROOT_DIR}"
+        ENV ROCM_PATH
+        ENV HIP_PATH
+        /opt/rocm
+        /opt/rocm/hip
+        ${CMAKE_PREFIX_PATH}
+        PATH_SUFFIXES bin
+        NO_DEFAULT_PATH
+        )
+    if(NOT HIP_HCC_EXECUTABLE)
+        # Now search in default paths
+        find_program(HIP_HCC_EXECUTABLE hcc)
+    endif()
+    mark_as_advanced(HIP_HCC_EXECUTABLE)
   endif()
 endif()
