7051c71eab164a15edd488cf1d65f8c1c5cb63e5
Nico Trost <nico.trost@amd.com>
2019-07-22 09:03:40 +0000

    cmake update #1

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -26,74 +26,70 @@
 # Git
 find_package(Git REQUIRED)
 
-# DownloadProject package
-include(cmake/DownloadProject/DownloadProject.cmake)
-
 # Workaround until hcc & hip cmake modules fixes symlink logic in their config files.
 # (Thanks to rocBLAS devs for finding workaround for this problem!)
 list(APPEND CMAKE_PREFIX_PATH /opt/rocm/hcc /opt/rocm/hip /opt/rocm)
 
+# Find OpenMP package
+find_package(OpenMP)
+if (NOT OPENMP_FOUND)
+  message("-- OpenMP not found. Compiling WITHOUT OpenMP support.")
+else()
+  option(SUPPORT_OMP "Compile WITH OpenMP support." ON)
+endif()
+
+# MPI
+find_package(MPI)
+if (NOT MPI_FOUND)
+  message("-- MPI not found. Compiling WITHOUT MPI support.")
+else()
+  option(SUPPORT_MPI "Compile WITH MPI support." OFF)
+  if (SUPPORT_MPI)
+    set(CMAKE_C_COMPILER ${MPI_COMPILER})
+    set(CMAKE_CXX_COMPILER ${MPI_COMPILER})
+  endif()
+endif()
+
+# Find HIP package
+find_package(HIP 1.5.19211) # ROCm 2.5
+
+if (NOT HIP_FOUND)
+  message("-- HIP not found. Compiling WITHOUT HIP support.")
+else()
+  option(SUPPORT_HIP "Compile WITH HIP support." ON)
+endif()
+
+# HIP related library dependencies
 if(SUPPORT_HIP)
-  # HIP configuration
-  # Ignore hcc warning: argument unused during compilation: '-isystem /opt/rocm/hip/include'
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-unused-command-line-argument")
-  find_package(HIP REQUIRED CONFIG PATHS ${CMAKE_PREFIX_PATH})
-  set(CMAKE_SHARED_LIBRARY_CREATE_HIP_FLAGS ${CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS})
-  set(CMAKE_HIP_COMPILE_OPTIONS_PIC ${CMAKE_C_COMPILE_OPTIONS_PIC})
-  set(CMAKE_HIP_COMPILE_OPTIONS_PIE ${CMAKE_C_COMPILE_OPTIONS_PIE})
-  set(CMAKE_HIP_COMPILE_OPTIONS_DLL ${CMAKE_C_COMPILE_OPTIONS_DLL})
-  set(CMAKE_SHARED_LIBRARY_HIP_FLAGS ${CMAKE_SHARED_LIBRARY_C_FLAGS})
-  set(CMAKE_SHARED_LIBRARY_LINK_HIP_FLAGS ${CMAKE_SHARED_LIBRARY_LINK_C_FLAGS})
-  set(CMAKE_SHARED_LIBRARY_RUNTIME_HIP_FLAG ${CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG})
-  set(CMAKE_SHARED_LIBRARY_RUNTIME_HIP_FLAG_SEP ${CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG_SEP})
-  set(CMAKE_SHARED_LIBRARY_RPATH_LINK_HIP_FLAG ${CMAKE_SHARED_LIBRARY_RPATH_LINK_C_FLAG})
-  set(CMAKE_EXE_EXPORTS_HIP_FLAG ${CMAKE_EXE_EXPORTS_C_FLAG})
-  set(CMAKE_SHARED_LIBRARY_SONAME_HIP_FLAG ${CMAKE_SHARED_LIBRARY_SONAME_C_FLAG})
-  set(CMAKE_EXECUTABLE_RUNTIME_HIP_FLAG ${CMAKE_SHARED_LIBRARY_RUNTIME_CXX_FLAG})
-  set(CMAKE_EXECUTABLE_RUNTIME_HIP_FLAG_SEP ${CMAKE_SHARED_LIBRARY_RUNTIME_CXX_FLAG_SEP})
-  set(CMAKE_EXECUTABLE_RPATH_LINK_HIP_FLAG ${CMAKE_SHARED_LIBRARY_RPATH_LINK_CXX_FLAG})
-  set(CMAKE_SHARED_LIBRARY_LINK_HIP_WITH_RUNTIME_PATH ${CMAKE_SHARED_LIBRARY_LINK_C_WITH_RUNTIME_PATH})
-  set(CMAKE_INCLUDE_FLAG_HIP ${CMAKE_INCLUDE_FLAG_C})
-  set(CMAKE_INCLUDE_FLAG_SEP_HIP ${CMAKE_INCLUDE_FLAG_SEP_C})
-  set(CMAKE_HIP_CREATE_SHARED_LIBRARY
-      "${HIP_HIPCC_EXECUTABLE} ${HIP_LIBRARY_FLAGS} <CMAKE_SHARED_LIBRARY_CXX_FLAGS> <LANGUAGE_COMPILE_FLAGS> <LINK_FLAGS> <CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS> <SONAME_FLAG><TARGET_SONAME> -o <TARGET> <OBJECTS> <LINK_LIBRARIES>")
+  find_package(rocblas 2.2.6 REQUIRED) # ROCm 2.5
+  find_package(rocsparse 1.1.5 REQUIRED) # ROCm 2.5
+  find_package(rocprim 2.5.0 REQUIRED) # ROCm 2.5
 endif()
 
-# Test dependencies
-if(BUILD_TEST)
-  if(NOT DEPENDENCIES_FORCE_DOWNLOAD)
-    find_package(GTest QUIET)
-  endif()
-  if(NOT GTEST_FOUND)
-    message(STATUS "GTest not found. Downloading and building GTest.")
-    set(GTEST_ROOT ${CMAKE_CURRENT_BINARY_DIR}/gtest CACHE PATH "")
-    download_project(PROJ        googletest
-             GIT_REPOSITORY      https://github.com/google/googletest.git
-             GIT_TAG             release-1.8.1
-             INSTALL_DIR         ${GTEST_ROOT}
-             CMAKE_ARGS          -DBUILD_GTEST=ON -DINSTALL_GTEST=ON -Dgtest_force_shared_crt=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-             LOG_DOWNLOAD        TRUE
-             LOG_CONFIGURE       TRUE
-             LOG_BUILD           TRUE
-             LOG_INSTALL         TRUE
-             BUILD_PROJECT       TRUE
-             UPDATE_DISCONNECTED TRUE
-    )
-  endif()
-  find_package(GTest REQUIRED)
-endif()
-
-# ROCm package
+# ROCm cmake package
+set(PROJECT_EXTERN_DIR ${CMAKE_CURRENT_BINARY_DIR}/extern)
 find_package(ROCM QUIET CONFIG PATHS ${CMAKE_PREFIX_PATH})
 if(NOT ROCM_FOUND)
   set(rocm_cmake_tag "master" CACHE STRING "rocm-cmake tag to download")
   file(DOWNLOAD https://github.com/RadeonOpenCompute/rocm-cmake/archive/${rocm_cmake_tag}.zip
-       ${CMAKE_CURRENT_BINARY_DIR}/rocm-cmake-${rocm_cmake_tag}.zip
-  )
-  execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${CMAKE_CURRENT_BINARY_DIR}/rocm-cmake-${rocm_cmake_tag}.zip
-                  WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-  )
-  find_package(ROCM REQUIRED CONFIG PATHS ${CMAKE_CURRENT_BINARY_DIR}/rocm-cmake-${rocm_cmake_tag})
+       ${PROJECT_EXTERN_DIR}/rocm-cmake-${rocm_cmake_tag}.zip STATUS status LOG log)
+
+  list(GET status 0 status_code)
+  list(GET status 1 status_string)
+
+  if(NOT status_code EQUAL 0)
+    message(FATAL_ERROR "error: downloading
+    'https://github.com/RadeonOpenCompute/rocm-cmake/archive/${rocm_cmake_tag}.zip' failed
+    status_code: ${status_code}
+    status_string: ${status_string}
+    log: ${log}
+    ")
+  endif()
+
+  execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${PROJECT_EXTERN_DIR}/rocm-cmake-${rocm_cmake_tag}.zip
+                  WORKING_DIRECTORY ${PROJECT_EXTERN_DIR})
+
+  find_package(ROCM REQUIRED CONFIG PATHS ${PROJECT_EXTERN_DIR}/rocm-cmake-${rocm_cmake_tag})
 endif()
 
 include(ROCMSetupVersion)
